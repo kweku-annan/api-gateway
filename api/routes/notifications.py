3961a7de5975074a8f2d941b76fde6a9a751e5cc
kweku-annan <kwekuannan.dev@gmail.com>
2025-11-11 09:22:56 +0000

    feat: Define send_email route

--- api/routes/notifications.py
+++ api/routes/notifications.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 """Core endpoint for notifications requests and responses management."""
 from flask import Blueprint, request, jsonify, current_app
+from api.middleware.auth import require_path
+from api.middleware.rate_limiter import rate_limit
+from api.models.request_models import NotificationRequest
+from api.models.request_models import StandardResponse
 
+from api.utils.validators import validate_notification_data
 
 # Create notification blueprint
 notifications_bp = Blueprint('notifications', __name__)
 
 # Endpoint to send email notification
+@require_auth
+@rate_limit
 @notifications_bp.route('/email', methods=['POST'])
 def send_email():
     """Send email notification."""
     data = request.get_json()
+
+    # Validate for required fields
+    validate_error = validate_notification_data(data)
+    if validate_error:
+        return StandardResponse.error('validate_error', validate_error),
--- api/utils/validators.py
+++ api/utils/validators.py
@@ -7,12 +7,12 @@ def validate_notification_data(data):
     -- Checks for required fields and correct data types.
     """
     if not data.get("user_id"):
-        return "'user_id' is required."
+        return "'user_id' is required.", False
     if not isinstance(data.get("user_id"), str):
-        return "'user_id' must be a string."
+        return "'user_id' must be a string.", False
     if not data.get("template_id"):
-        return "'template_id' is required."
+        return "'template_id' is required.", False
     if not isinstance(data.get("template_id"), str):
-        return "'template_id' must be a string."
+        return "'template_id' must be a string.", False
 
     return None
